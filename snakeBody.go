8d05016de3559a37220d56bc769f383b577879f0
horirashid <hawry.jawad@gmail.com>
2021-07-22 17:37:44 +0900

    create a queue from scratch instead of using the built-in package

--- snakeBody.go
+++ snakeBody.go
@@ -1,12 +1,63 @@
 package main
 
 import (
-	"container/list"
+	"errors"
 	"fmt"
 )
 
 type Point struct {
 	x, y int
+}
+type Queue struct {
+	pos   []*Point
+	count int
+	tail  int
+	head  int
+}
+
+func (q *Queue) Push(p *Point) (err error) {
+	if q.isEmpty() {
+		q.resetQueue()
+	}
+	q.head++
+	q.count++
+	q.pos = append(q.pos, p)
+	return
+}
+
+func (q *Queue) Pop() (p *Point, err error) {
+	if q.isEmpty() {
+		fmt.Println("queue empty")
+		err = errors.New("queue empty")
+		return
+	}
+	p = q.pos[q.tail]
+	q.tail++
+	q.count--
+	return p, nil
+}
+
+func (q *Queue) List() (err error) {
+	if q.isEmpty() {
+		fmt.Println("queue empty")
+		err = errors.New("queue empty")
+		return
+	}
+	for i := q.tail; i <= q.head; i++ {
+		fmt.Printf("queue[%d]=%d\n", i, q.pos[i])
+	}
+	fmt.Println("end of queue")
+	return
+}
+
+func (q *Queue) resetQueue() {
+	q.head = -1
+	q.tail = -1
+	q.count = 0
+	q.pos = q.pos[:0]
+}
+func (q *Queue) isEmpty() bool {
+	return q.count == 0
 }
 
 func main() {
@@ -14,24 +65,21 @@ func main() {
 	p2 := &Point{4,5}
 	p3 := &Point{3,5}
 	p4 := &Point{2,5}
-	queue := list.New()
-	queue.PushBack(p1) // push new Point object into the queue
-	queue.PushBack(p2)
-	queue.PushBack(p3)
-	queue.PushBack(p4)
 
-	// change value of one pointer
-	tmp := queue.Front().Value.(*Point)
-	tmp.x = tmp.x+1
-
-	// iterate through the queue
-	// print all values
-	// remove all elements
-	for queue.Len() > 0 {
-		obj := queue.Front() // view the first object from the queue
-		xy := obj.Value.(*Point) // convert it to the Point object
-		fmt.Printf("x: %d, y: %d\n", xy.x, xy.y)
-		queue.Remove(obj) // Dequeue
+	q := &Queue{
+		pos:   []*Point{p1,p2},
+		head:  1,
+		count: 2,
+		tail:  0,
 	}
-
+	q.List()
+	q.Push(p3)
+	p,_ := q.Pop()
+	fmt.Printf("the popped value is: x=%d, y=%d\n",p.x, p.y)
+	q.List()
+	q.Push(p4)
+	q.Pop()
+	q.List()
+	q.resetQueue()
+	q.List()
 }
