76e6845a59bf251bd11b47823ed5231d3d397327
M215821_薛明昊 <setsumeihiroshi@gmail.com>
2021-07-29 11:46:02 +0000

    薛明昊_Commit

---
+++ commit_XUE.sh
@@ -0,0 +1,6 @@
+git config --global user.name "M215821_薛明昊" # コミットログに残る名前
+git config --global user.email "setsumeihiroshi@gmail.com" # コミットログに残るメールアドレス
+git config --global core.pager "" # ページャーを「なし」．つまり標準出力を選択（標準は less）
+git add .
+git commit -m "薛明昊_Commit"
+git push -u orgin develop
